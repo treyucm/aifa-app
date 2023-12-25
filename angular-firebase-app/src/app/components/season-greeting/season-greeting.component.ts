3e22ab0b1873cb2a21a221f44758a3a999e37404
treyucm <tun83170@ucmo.edu>
2023-12-25 01:25:03 -0600

    modified:   .firebase/fir-app-demo-acd72/hosting/3rdpartylicenses.txt 	new file:   .firebase/fir-app-demo-acd72/hosting/assets/favicon/android-chrome-192x192.png 	new file:   .firebase/fir-app-demo-acd72/hosting/assets/favicon/android-chrome-512x512.png 	new file:   .firebase/fir-app-demo-acd72/hosting/assets/favicon/apple-touch-icon.png 	new file:   .firebase/fir-app-demo-acd72/hosting/assets/favicon/favicon-16x16.png 	new file:   .firebase/fir-app-demo-acd72/hosting/assets/favicon/favicon-32x32.png 	new file:   .firebase/fir-app-demo-acd72/hosting/assets/favicon/favicon.ico 	new file:   .firebase/fir-app-demo-acd72/hosting/assets/images/Frame1.png 	new file:   .firebase/fir-app-demo-acd72/hosting/assets/images/Frame3.png 	new file:   .firebase/fir-app-demo-acd72/hosting/assets/images/snowy.png 	deleted:    .firebase/fir-app-demo-acd72/hosting/favicon.ico 	modified:   .firebase/fir-app-demo-acd72/hosting/index.html 	deleted:    .firebase/fir-app-demo-acd72/hosting/main.2027659ede1ee885.js 	new file:   .firebase/fir-app-demo-acd72/hosting/main.7714b98d33e1554e.js 	new file:   .firebase/fir-app-demo-acd72/hosting/snowy.7a5e66f5bc903d61.png 	renamed:    .firebase/fir-app-demo-acd72/hosting/styles.889828dd37d1ea33.css -> .firebase/fir-app-demo-acd72/hosting/styles.b94484fa2760f3cb.css 	modified:   .firebase/hosting.LmZpcmViYXNlXGZpci1hcHAtZGVtby1hY2Q3Mlxob3N0aW5n.cache 	modified:   src/app/components/season-greeting/season-greeting.component.css 	modified:   src/app/components/season-greeting/season-greeting.component.html 	modified:   src/app/components/season-greeting/season-greeting.component.ts

--- angular-firebase-app/src/app/components/season-greeting/season-greeting.component.ts
+++ angular-firebase-app/src/app/components/season-greeting/season-greeting.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class SeasonGreetingComponent {
   public userName = "Test User";
+  public ReadMore = false;
+  public visible = false;
+  ShowGif = {
+    current: false,
+  }
+    onclick()
+  {
+    this.ReadMore = !this.ReadMore; //not equal to condition
+    this.visible = !this.visible
+  }
 }
